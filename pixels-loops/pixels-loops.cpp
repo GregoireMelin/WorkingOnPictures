95f0021666398942364b36997f8b57a1c67286dc
MELIN Gregoire <gremelin@glouglou.pedago.ipb.fr>
2017-10-21 18:25:16 +0200

    commit

--- pixels-loops/pixels-loops.cpp
+++ pixels-loops/pixels-loops.cpp
@@ -5,6 +5,10 @@
 using namespace cv;
 using namespace std;
 
+double e1;
+double e2;
+double period;
+
 void process(const char* imsname)
 {
   if(imsname==NULL)
@@ -20,11 +24,39 @@ void process(const char* imsname)
     if(image.empty())
       cout<<"Image vide"<<endl;
     else
-      resize(image,imageResized,Size(),100,100);
-      imwrite("resized.png",imageResized);
-      namedWindow("resized.png",WINDOW_AUTOSIZE);
-      imshow("resized.png",imageResized);
-  }
+
+      resize(image,imageResized,Size(),80,80);
+
+
+      // Methode at
+  /*    e1=(double)getTickCount();
+	     for ( int j = 0; j < imageResized.rows; ++j)
+       {
+		     for ( int i = 0; i < imageResized.cols; ++i)
+	        {
+		          Vec3b * pixel = &imageResized.at<Vec3b>(j, i);
+		          op(&(*pixel)[0]);
+              op(&(*pixel)[1]);
+              op(&(*pixel)[2]);
+	        }
+        }
+      e2=(double)getTickCount();
+      period = (e2 - e1)/getTickFrequency();
+
+      cout << period <<endl;
+
+	// iterator
+	t = (double)(getTickCount());
+	Mat_<Vec3b>::iterator it, end;
+	for (it = img_out2.begin<Vec3b>(), end = img_out2.end<Vec3b>(); it != end; ++it)
+	{
+		op(&(*it)[0]);
+		op(&(*it)[1]);
+		op(&(*it)[2]);
+	}
+	t = ((double)getTickCount() - t)/getTickFrequency();
+	cout<<"Time with iterator: " << t*1000 << "ms" <<endl;*/
+}
 }
 
 void usage (const char *s)
