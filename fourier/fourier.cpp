9c0bee087c9ce9fdd8898fb0f43b9285108bd936
MELIN Gregoire <gremelin@ecstazy.pedago.ipb.fr>
2017-11-21 18:28:43 +0100

    Fourier

--- fourier/fourier.cpp
+++ fourier/fourier.cpp
@@ -7,6 +7,85 @@ using namespace std;
 
 Mat image;
 
+void fshift(Mat phaseI)
+{
+	phaseI = phaseI(Rect(0, 0, phaseI.cols & -2, phaseI.rows & -2));
+	int cx = phaseI.cols/2;
+	int cy = phaseI.rows/2;
+	Mat q0(phaseI, Rect(0, 0, cx, cy));
+	Mat q1(phaseI, Rect(cx, 0, cx, cy));
+	Mat q2(phaseI, Rect(0, cy, cx, cy));
+	Mat q3(phaseI, Rect(cx, cy, cx, cy));
+	Mat tmp;
+	q0.copyTo(tmp);
+	q3.copyTo(q0);
+	tmp.copyTo(q3);
+	q1.copyTo(tmp);
+	q2.copyTo(q1);
+	tmp.copyTo(q2);
+	normalize(phaseI, phaseI, 0, 1, NORM_MINMAX);
+}
+
+
+Mat getIDFT(Mat complexI)
+{
+  Mat nouvelle;
+  idft(complexI, nouvelle);
+  Mat planess[] = {Mat::zeros(complexI.size(), CV_32F), Mat::zeros(complexI.size(), CV_32F)};
+  split(nouvelle, planess);
+  magnitude(planess[0], planess[1], nouvelle);
+  normalize(nouvelle, nouvelle, 0, 1, NORM_MINMAX);
+  return(nouvelle);
+}
+
+
+Mat getPhaseSpec(Mat complex, bool shift = true)
+{
+      Mat phaseI;
+      Mat planes[] = {Mat::zeros(complex.size(), CV_32F), Mat::zeros(complex.size(), CV_32F)};
+      split(complex, planes);
+      phase(planes[0], planes[1], phaseI);
+      phaseI += Scalar::all(1);
+      //Passage en log
+       log(phaseI, phaseI);
+      //Crop le spectre pour qu'il ait un nombre pair de colonnes
+      if(shift)
+		 	  fshift(phaseI);
+
+      return phaseI;
+}
+
+
+Mat getMagSpec(Mat complex,bool shift = true)
+{
+    Mat magI;
+    Mat planes[] = {Mat::zeros(complex.size(), CV_32F), Mat::zeros(complex.size(), CV_32F)};
+    split(complex, planes);
+    magnitude(planes[0], planes[1], magI);
+    magI += Scalar::all(1);
+    log(magI, magI);
+    //Crop le spectre pour qu'il ait un nombre pair de colonnes
+    if(shift)
+			fshift(magI);
+
+
+  return magI;
+}
+
+Mat getDFT(Mat image)
+{
+  Mat imageZeroPadded;
+  int m = getOptimalDFTSize( image.rows );
+  int n = getOptimalDFTSize( image.cols );
+  copyMakeBorder(image, imageZeroPadded, 0, m - image.rows, 0, n - image.cols, BORDER_CONSTANT, Scalar::all(0));
+
+  Mat planes[] = {Mat_<float>(imageZeroPadded), Mat::zeros(imageZeroPadded.size(), CV_32F)};
+  Mat complexI;
+  merge(planes, 2, complexI);
+  dft(complexI, complexI);
+  split(complexI, planes);
+  return complexI;
+}
 void process(const char* ims, double freq)
 {
   if(ims==NULL)
@@ -17,79 +96,44 @@ void process(const char* ims, double freq)
   }
     else
   {
+       //Lecture de l'image
        image=imread(ims,0);
-
-       //Optimisation taille image via zero padding
-       Mat imageZeroPadded;
-       int m = getOptimalDFTSize( image.rows );
-       int n = getOptimalDFTSize( image.cols );
-       copyMakeBorder(image, imageZeroPadded, 0, m - image.rows, 0, n - image.cols, BORDER_CONSTANT, Scalar::all(0));
-
-       Mat planes[] = {Mat_<float>(imageZeroPadded), Mat::zeros(imageZeroPadded.size(), CV_32F)};
-       Mat complexI;
-       merge(planes, 2, complexI);
-       dft(complexI, complexI);
-       split(complexI, planes);                   // planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
-       Mat magI,phaseI;
+       Mat magI,phaseI, complexI, nouvelle;
+       //Calcul de la TFD
+       complexI=getDFT(image);
+       Mat planes[] = {Mat::zeros(complexI.size(), CV_32F), Mat::zeros(complexI.size(), CV_32F)};
        cartToPolar(planes[0],planes[1],magI,phaseI,true);
+       magI=getMagSpec(complexI);
+       phaseI=getPhaseSpec(complexI);
 
+       //Mat mask;
+       //Mat planess[] = {Mat::zeros(complexI.size(), CV_32F),Mat::zeros(complexI.size(), CV_32F)};
+       //Mat kernel_spec;
+       //planess[0] = mask; // real
+       //planess[1] = mask; // imaginar
+       //merge(planess, 2, kernel_spec);
 
-//CALCUL GAIN
-       //Passage en log
-       log(magI, magI);
-       //Crop le spectre pour qu'il ait un nombre pair de colonnes
-       magI = magI(Rect(0, 0, magI.cols & -2, magI.rows & -2));
-       int cx = magI.cols/2;
-       int cy = magI.rows/2;
-       //Recentrage
-       Mat q0(magI, Rect(0, 0, cx, cy));
-       Mat q1(magI, Rect(cx, 0, cx, cy));
-       Mat q2(magI, Rect(0, cy, cx, cy));
-       Mat q3(magI, Rect(cx, cy, cx, cy));
+       //mulSpectrums(complexI, kernel_spec, complexI, DFT_ROWS);
+       /*float maxMag=planes[0];
+       for(int i=0;i<sizeof(planes)/sizeof(planes[0]);i++)
+       {
+         if(planes > maxMag)
+         {
+           maxMag=planes[0,i];
+         }
+       }*/
 
-       Mat tmp;
-       q0.copyTo(tmp);
-       q3.copyTo(q0);
-       tmp.copyTo(q3);
-
-       q1.copyTo(tmp);
-       q2.copyTo(q1);
-       tmp.copyTo(q2);
-       //transforme en matrice lisible
-       normalize(magI, magI, 0, 1, NORM_MINMAX);
-
-//CALCUL PHASE
-      //Passage en log
-      log(phaseI, phaseI);
-      //Crop le spectre pour qu'il ait un nombre pair de colonnes
-      phaseI = phaseI(Rect(0, 0, phaseI.cols & -2, phaseI.rows & -2));
-      int cxx = phaseI.cols/2;
-      int cyy = phaseI.rows/2;
-      //Recentrage
-      Mat q4(phaseI, Rect(0, 0, cxx, cyy));
-      Mat q5(phaseI, Rect(cxx, 0, cxx, cyy));
-      Mat q6(phaseI, Rect(0, cyy, cxx, cyy));
-      Mat q7(phaseI, Rect(cxx, cyy, cxx, cyy));
-
-      Mat tmp2;
-      q4.copyTo(tmp2);
-      q7.copyTo(q4);
-      tmp.copyTo(q7);
-
-      q5.copyTo(tmp2);
-      q6.copyTo(q5);
-      tmp.copyTo(q6);
-
-      //transforme en matrice lisible
-      normalize(phaseI, phaseI, 0, 1, NORM_MINMAX);
-
+			 freq=freq+1;
+       nouvelle=getIDFT(complexI);
 //AFFICHAGES ET ENREGISTREMENT
        imshow(ims,image);
        imwrite("magnitude.png", magI);
        imwrite("phase.png",phaseI);
-
        imshow("spectrum magnitude", magI);
        imshow("spectrum phase",phaseI);
+			 imwrite("magnitude-modify.png", magI);
+       imshow("IDFT", nouvelle);
+       imwrite("inverse-modify.png",nouvelle);
        waitKey(0);
   }
 }
