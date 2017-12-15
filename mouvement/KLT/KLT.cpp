832fc7d6e76b817e285de73bd8f3e7bb01694ace
MELIN Gregoire <gremelin@full.pedago.ipb.fr>
2017-12-15 09:15:40 +0100

    MAJ KLP

--- mouvement/KLT/KLT.cpp
+++ mouvement/KLT/KLT.cpp
@@ -5,98 +5,97 @@
 #include "opencv2/video/background_segm.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/video/tracking.hpp"
-#include <iostream> 
-#include <fstream> 
+#include <iostream>
+#include <fstream>
 #include <cstdlib>
 #include <vector>
-
-
 
 using namespace cv;
 using namespace std;
 
-
 int main( int argc, char** argv )
 {
-
-  if( argc <1 )  {
+  if( argc <1 )
+  {
     printf("Usage: main video_name \n");
-    exit(0);     
+    exit(0);
   }
 
+  //---- OUVERTURE DE LA VIDEO ---- //
 
-  VideoCapture capture(argv[1]);
-
-
-
-  if(!capture.isOpened()) {  // check if we succeeded
+  VideoCapture capture(argv[1]); //1er argument = nom de la video chargee
+  if(!capture.isOpened())
+  {
     cout << "error opening video file" << endl;
     return -1;
   }
-
-
-
+  //creation de la fenetre acceuillant l'affichage
   namedWindow("tracking",1);
 
 
-  Mat frame;
-  Mat frameg, prev_frameg;
-  capture >> frame; // get a new fame 
-  cvtColor(frame, frameg, CV_BGR2GRAY);
-  prev_frameg = frameg.clone();  
+  // ---- TRAITEMENT DE LA VIDEO : TRACKING DES POINTS D'INTERETS ---- //
 
-  std::vector<Point2f> features_prev, features;
-  goodFeaturesToTrack(frameg, // the image 
-			  features,   // the output detected features
-			  50,  // the maximum number of features 
-			  0.01,     // quality level
-			  10      // min distance between two features
-			  );
 
-  
+  /* TRAVAIL SUR LA PREMIERE IMAGE */
 
+  Mat frame; //Stockage de la frame courante et de la frame precedente en niveau de gris
+  Mat frameg, prev_frameg;//dans les objets frameg et prev_frameg
+  capture >> frame;
+  cvtColor(frame, frameg, CV_BGR2GRAY); //Conversion BGR en niveau de gris
+  prev_frameg = frameg.clone();
+
+  std::vector<Point2f> features_prev, features; //Creation des objets acceuillant les points d'interet d'une image
+
+  //DETECTION DES MOTIFS STOCKES DANS features
+  goodFeaturesToTrack(frameg,features,50,0.01,10);
   size_t i;
   for( i = 0; i < features.size(); i++ )
-    {
-      circle( frame, features[i], 3, Scalar(0,255,0), -1, 8);
-    }
-  imshow("tracking", frame);
+    circle( frame, features[i], 3, Scalar(0,255,0), -1, 8);   //On marque d'un cercle les feature reperees sur la premiere image de la video
+// ---------------------------------------------
+  //Fonction goodFeaturesToTrack() :
+  //The function finds the most prominent corners in the image or in the specified image region.
+    //frameg : the image
+    //features : the output detected features
+    //50 : the maximum number of features
+    //0.01 : quality level
+    //10 : min distance between two features
+// ---------------------------------------------
+
+  imshow("tracking", frame); //Affichage de la frame travaillee
   waitKey();
 
+  /* TRACKING DES POINTS D'INTERETS SUR LA SUITE DE LA VIDEO */
   double nframes= capture.get(CV_CAP_PROP_FRAME_COUNT);
   for (int f=1; f<nframes; f++)
     {
-
       capture >> frame; // get a new frame from camera
       cvtColor(frame, frameg, CV_BGR2GRAY);
       features_prev = features;
-      
-      // Find position of features in new image
+
+      // Calculates an optical flow for a sparse feature set using the iterative Lucas-Kanade method with pyramids.
       vector<uchar> status;
       vector<float> err;
-      calcOpticalFlowPyrLK(prev_frameg, frameg, features_prev, features,
-			   status,    // tracking success
-			   err      // tracking error
-			   );
+      // Find position of features in new image
+      calcOpticalFlowPyrLK(prev_frameg, frameg, features_prev, features, status,err);
+			//err : tracking error
+      //status : tracking success
+      prev_frameg = frameg.clone();
 
-      prev_frameg = frameg.clone();  
-      size_t i;
+      //Dessiner un point VERT sur la feature detectee
+      size_t i; //type qui peut contenir un nombre assez grand pour représenter la taille du plus grand objet creable ~ u_int
       int size = 0;
       for( i = 0; i < features.size(); i++ )
-	{
-	  if( !status[i] )
-	    continue;
-	  size++;
-	  circle( frame, features[i], 3, Scalar(0,255,0), -1, 8);
-	}
+	    {
+	       if(!status[i])
+	         continue;
+	       size++;
+	       circle( frame, features[i], 3, Scalar(0,255,0), -1, 8);
+	    }
+
+      //Affichage de la frame courante avec les features pointees
       imshow("tracking", frame);
       cout << "Number of tracked features " << size << endl;
-      waitKey();
 
+      //waitKey(); //Passage a la frame suivante
     }
-
-
-
 }
-
-
--- mouvement/KLT/KLT.pro
+++ mouvement/KLT/KLT.pro
@@ -1,12 +1,19 @@
 MOC_DIR = .moc
-OBJECTS_DIR = .obj
+OBJECT_DIR = .obj
 
-TEMPLATE = app
-CONFIG += qt warn_on release thread
-SOURCES = KLT.cpp
-TARGET = KLT
-CONFIG -= app_bundle
+TEMPLATE   = app
+CONFIG  += qt warn_on release thread
+SOURCES  = KLT.cpp
+TARGET   = KLT
+CONFIG  -= app_bundle
 
-INCLUDEPATH += /usr/include/opencv2/
+enseirb {
+INCLUDEPATH += /opt/opencv/include/opencv2
+QMAKE_LFLAGS += -Wl,-R/opt/opencv/lib -L/opt/opencv/lib
+}
 
-LIBS = -L/usr/local/lib/ -lopencv_highgui -lopencv_imgproc -lopencv_core -lopencv_video 
+LIBS += \
+-lopencv_core \
+-lopencv_highgui \
+-lopencv_imgproc \
+-lopencv_video
